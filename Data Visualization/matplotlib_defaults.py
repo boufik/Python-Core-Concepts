234910f4fa8a5450f01de9b8a3202ca02e65236f
Thomas Boufikos <106670035+boufik@users.noreply.github.com>
2024-10-24 11:55:39 +0300

    Create matplotlib_defaults.py

---
+++ Data Visualization/matplotlib_defaults.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True)
+plt.rc("axes", labelweight="bold", labelsize="large", titleweight="bold", titlesize=14, titlepad=10)
